8ae0c70dbfddcf4cd22613469f1aa901c3ce5105
leogasmus <leogasmus@gmail.com>
2020-03-23 23:13:07 +0200

    Add review carousel

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,13 @@
 $('.rooms-carousel').slick({
     autoplay: true,
     speed: 1000,
+    fade: true,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     dots: true,
-    appendDots: $('.carousel-dots')
+    appendDots: $('.carousel-room__dots')
   });
 $('.btn-prev').on('click', function(){
     $('.rooms-carousel').slick('slickPrev');
@@ -15,3 +16,21 @@ $('.btn-next').on('click', function(){
     $('.rooms-carousel').slick('slickNext');
 })
 //  Review carousel
+$('.review-carousel').slick({
+    /* autoplay: true,
+    speed: 1000,
+    fade: true, */
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    appendDots: $('.carousel-review__dots')
+  });
+$('.btn-prev').on('click', function(){
+    $('.review-carousel').slick('slickPrev');
+})
+$('.btn-next').on('click', function(){
+    $('.review-carousel').slick('slickNext');
+})
